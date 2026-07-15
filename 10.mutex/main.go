b25fae4a4977e80bbe63108705d8cd8c3c797c3f
Prince Saliya <princesaliya4040@gmail.com>
2026-07-15 16:50:03 +0530

    refactor: simplify processNum function and remove unnecessary getprocessedNum

--- 10.mutex/main.go
+++ 10.mutex/main.go
@@ -3,29 +3,20 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 var lock sync.Mutex
 
-func getprocessedNum(num int) int {
-	time.Sleep(1 * time.Second)
-	return num * 2
-}
-
-func processNum(wg *sync.WaitGroup, nums *[]int, num int) {
+func processNum(wg *sync.WaitGroup, num int, dest *[]int) {
 	defer wg.Done()
-
-	processedNum := getprocessedNum(num)
+	processedNum := num * 2
 
 	lock.Lock()
-	*nums = append(*nums, processedNum)
+	*dest = append(*dest, processedNum)
 	lock.Unlock()
 }
 
 func main() {
-	start := time.Now()
-
 	nums := []int{1, 2, 3, 4, 5}
 	result := []int{}
 
@@ -33,11 +24,9 @@ func main() {
 
 	for _, num := range nums {
 		wg.Add(1)
-		go processNum(&wg, &result, num)
+		go processNum(&wg, num, &result)
 	}
 
 	wg.Wait()
 	fmt.Println(result)
-
-	fmt.Println(time.Since(start))
 }
--- 11.confinement/main.go
+++ 11.confinement/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var lock sync.Mutex
+
+func processNum(wg *sync.WaitGroup, num int, dest *[]int) {
+	defer wg.Done()
+	processedNum := num * 2
+
+	lock.Lock()
+	*dest = append(*dest, processedNum)
+	lock.Unlock()
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	result := []int{}
+
+	var wg sync.WaitGroup
+
+	for _, num := range nums {
+		wg.Add(1)
+		go processNum(&wg, num, &result)
+	}
+
+	wg.Wait()
+	fmt.Println(result)
+}
